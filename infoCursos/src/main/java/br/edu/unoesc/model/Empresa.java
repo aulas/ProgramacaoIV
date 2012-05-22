ca7e60b3daf12cd8e9f6dbc23e07e0b29fa7a1db
bruno zanuzzo <brunorobertozanuzzo@gmail.com>
2012-05-22 19:39:10 -0300

    incluindo arquivos empresa e usuario

---
+++ infoCursos/src/main/java/br/edu/unoesc/model/Empresa.java
@@ -0,0 +1,71 @@
+package br.edu.unoesc.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Empresa {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long codigo;
+	private String nome;
+	private String cnpj;
+	private String site;
+	private String logo;
+	private String endereco;
+	private String telefone;
+	@ManyToOne
+	private Usuario usuario;
+	
+	public Long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCnpj() {
+		return cnpj;
+	}
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	public String getSite() {
+		return site;
+	}
+	public void setSite(String site) {
+		this.site = site;
+	}
+	public String getLogo() {
+		return logo;
+	}
+	public void setLogo(String logo) {
+		this.logo = logo;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+}
