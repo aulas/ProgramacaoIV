c2771fc9fd49bef50906c19aac80267a462f0ee5
rodrigofinger <rodrigofinger_@hotmail.com>
2012-05-22 21:24:09 -0300

    classe estado

--- infoCursos/src/main/java/br/edu/unoesc/model/Estado.java
+++ infoCursos/src/main/java/br/edu/unoesc/model/Estado.java
@@ -1,5 +1,36 @@
 package br.edu.unoesc.model;
 
-public class Estado {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Estado {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long codigo;
+	private String nome;
+	public Long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Estado(Long codigo, String nome) {
+		super();
+		this.codigo = codigo;
+		this.nome = nome;
+	}
+	public Estado() {
+		super();
+	}
+	
+	
 }
