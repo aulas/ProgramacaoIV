5de5f6767bcf014bf2e63ea6ef904a432e3f2b0c
Felipe <lipe_08@hotmail.com>
2012-05-29 21:17:39 -0300

    Destaques

--- infoCursos/src/main/java/br/edu/unoesc/model/Destaque.java
+++ infoCursos/src/main/java/br/edu/unoesc/model/Destaque.java
@@ -16,5 +16,36 @@ public class Destaque {
 	private Date termino;
 	private Periodo periodo;
 	private Curso curso;
-	private String teste;
+	
+	public Long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+	public Date getInicio() {
+		return inicio;
+	}
+	public void setInicio(Date inicio) {
+		this.inicio = inicio;
+	}
+	public Date getTermino() {
+		return termino;
+	}
+	public void setTermino(Date termino) {
+		this.termino = termino;
+	}
+	public Periodo getPeriodo() {
+		return periodo;
+	}
+	public void setPeriodo(Periodo periodo) {
+		this.periodo = periodo;
+	}
+	public Curso getCurso() {
+		return curso;
+	}
+	public void setCurso(Curso curso) {
+		this.curso = curso;
+	}
+	
 }
